f64c55c8580c379f874d273113098ab8092fd142
Chibueze Opata <c.opata@robosyslive.com>
2019-05-18 02:22:35 +0100

    Update readme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <!-- Favicon icon -->
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon.png">
         <title>Chingu Meteor Demo</title>
         <!-- Bootstrap Core CSS -->
         <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
